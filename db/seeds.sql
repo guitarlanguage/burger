65ff4fd2b0a1325e7bcdb52eb2daf6806457f3a9
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-13 20:53:17 -0500

    created directory and installed dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('A1 Burger');
+-- INSERT INTO burgers (burger_name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', true);
